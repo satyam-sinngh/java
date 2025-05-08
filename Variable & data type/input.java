165901ca04cc2d0268e31272b0ed99d92977d96d
Satyam Singh <satyamsinghbgs90@gmail.com>
2025-05-08 13:30:26 +0530

    handled resuource leakage

--- Variable & data type/input.java
+++ Variable & data type/input.java
@@ -5,11 +5,10 @@ public class input {
         // taking input from user
         System.out.println("ente the value of a & b");
         Scanner sc = new Scanner(System.in);
-        int a  = sc.nextInt();
+        int a = sc.nextInt();
         int b = sc.nextInt();
-        int sum = a+b;
+        int sum = a + b;
         System.out.println(sum);
-
+        sc.close();
     }
 }
- 
